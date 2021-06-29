04d0dace2b483e8aa492a624358b58e34ffb44d6
André Almeida <andrealmeid@collabora.com>
2021-06-29 16:17:42 -0300

    perf bench: Add futex2 benchmark tests
    Add support at the existing futex benchmarking code base to enable
    futex2 calls. `perf bench` tests can be used not only as a way to
    measure the performance of implementation, but also as stress testing
    for the kernel infrastructure.
    
    Signed-off-by: André Almeida <andrealmeid@collabora.com>
    (cherry picked from commit 42718e2912cdb805020a6c0dc97c52e8e6ba4525)

--- tools/arch/x86/include/asm/unistd_64.h
+++ tools/arch/x86/include/asm/unistd_64.h
@@ -17,3 +17,11 @@
 #ifndef __NR_setns
 #define __NR_setns 308
 #endif
+
+#ifndef __NR_futex_wait
+# define __NR_futex_wait 448
+#endif
+
+#ifndef __NR_futex_wake
+# define __NR_futex_wake 449
+#endif
